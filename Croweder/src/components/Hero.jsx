ff3224a449a0bb0d74e95cd6ef7a7c96748f5f4b
vinaykumar-hash <vinaykumar7525@gmail.com>
2025-06-13 16:22:58 +0530

    working on new register

--- Croweder/src/components/Hero.jsx
+++ Croweder/src/components/Hero.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Heromain from './Heromain'
 
 const Hero = () => {
   return (
     <div className='w-full h-lvh flex justify-start items-center flex-col gap-4'>
         <div className='w-full' style={{backgroundColor:"#537D5D"}}></div>
-        <div className=' bg-black/10' style={{height:"80%",width:"100%"}}></div>
+        <div className=' bg-black/10' style={{height:"80%",width:"100%"}}>
+          <Heromain/>
+        </div>
         <p className='fustat text-2xl '>Our Favourate's</p>
         <div className='flex justify-start items-center gap-4'>
             <div className='h-40 w-80 bg-black/10 rounded-xl'></div>
---
+++ Croweder/src/components/Heromain.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Heromain = () => {
+  return (
+    <div className='w-full h-full bg-black'>
+
+    </div>
+  )
+}
+
+export default Heromain
